fd5f59fc8903b3a82eb25650f4a43756f3a2e598
Hastak <hastakpatel19@gmail.com>
2022-09-07 21:13:10 +0530

    wrong delimiter handling testcase

--- code.cpp
+++ code.cpp
@@ -75,7 +75,6 @@ int testCase(string test) {
     if (test == ",") return 0;
     if (test == "1") return 1;
     if (test == "2") return 2;
-    if (test == ".2") return 2;
     if (test == "1,2") return 3;
     if (test == "1,2,a") return 4;
     if (test == "1,2,a,c") return 7;
@@ -85,6 +84,7 @@ int testCase(string test) {
     if (test == "-2") return -1; // signifies wrong testcase
     if (test == "-3") return -1;
     if (test == "-2,1,-3") return -1;
+    if (test == "1.2") return -1;
     else {
         cout<<"failed testcase"<<endl;
         return -1;
@@ -105,7 +105,7 @@ void result(int n1, int number) {
 int main() {
     
     StringCalculator obj;
-    string test = "-2,1,-3";
+    string test = "1.2";
     int number = obj.add(test);
     int n1 = testCase(test);
     result(n1,number);
