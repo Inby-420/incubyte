dc4119f92a16f054f62606ac5ec0fe7a3efb5ac1
Hastak <hastakpatel19@gmail.com>
2022-09-07 21:08:47 +0530

    passed test case (-2,1,-3)

--- code.cpp
+++ code.cpp
@@ -12,42 +12,56 @@ class StringCalculator {
     }
     int add(string numbers) {
         vector<int> temp;
-        int x = -1;
+        vector<int> negatives;
         int tens = 1;
-        try {
+        
+        bool flag = false;    
         for (int i=0; numbers[i] != '\0'; i++) {
             if (numbers[i] == ',') {
+                
                 int t1=0;
                 for (int k=temp.size()-1; k>=0; k--) {
                     t1 += temp[k]*tens;
                     tens = tens*10;
                 }
                 temp.clear();
-                
-                if (t1 <=1000) number += t1;
+                if (flag) negatives.push_back(t1);
+                else if (t1 <=1000) number += t1;
+                flag = false;
                 tens = 1;
             }
             else if (numbers[i] >=48 && numbers[i]<=57)
                 temp.push_back(numbers[i]-48);
                 
             else if (numbers[i] >=97 && numbers[i] <=122) temp.push_back(numbers[i]-96);
-            if (numbers[i] == '-') throw x;
+            if (numbers[i] == '-') flag = true;
             
         }
+        //if (negatives.size() >0) throw x;
         
-        
-        }
-        catch (int x) {
-            cout<<"Exception caught, -ve numbers not allowed"<<endl;
-            return -1;
-            
-        }
         int t2 = 0;
         for (int k=temp.size()-1; k>=0; k--) {
                     t2 += temp[k]*tens;
                     tens = tens*10;
         }
-        if (t2 <=1000) number += t2;
+        if (flag) negatives.push_back(t2);
+        else if (t2 <=1000) number += t2;
+        
+    
+        
+        try {
+            int x = -1;
+            if (negatives.size() >= 1) throw x;
+        }
+        catch (int x) {
+            cout<<"Exception caught, -ve numbers not allowed"<<endl;
+            cout<<"list of negatives: ";
+            for (auto num: negatives) cout<<num<<" ";
+            cout<<endl;
+            return -1;
+            
+        }
+        
         return number;
     }
     
@@ -69,6 +83,7 @@ int testCase(string test) {
     if (test == "1,10,100,1001") return 111;  
     if (test == "1,1002,100,2") return 103;
     if (test == "-2") return -1; // signifies wrong testcase
+    if (test == "-3") return -1;
     if (test == "-2,1,-3") return -1;
     else {
         cout<<"failed testcase"<<endl;
